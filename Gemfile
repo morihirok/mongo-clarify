33b56dbdb4a7d2664baf6cb543d09e48549372cd
morihirok <omnimani3@gmail.com>
2019-05-11 19:47:57 +0900

    Rewrite with pattern-match gem when ruby < 2.7

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 gem 'pry'
 gem 'pry-byebug'
+gem 'pattern-match'
 gem 'rspec'
 gem 'rubocop'
 gem 'rubocop-performance'
--- lib/mongo_clarify/investigate.rb
+++ lib/mongo_clarify/investigate.rb
@@ -1,31 +1,36 @@
 # frozen_string_literal: true
 
+require 'pattern-match/experimental'
+
 module MongoClarify
   class Investigate
+    using PatternMatch
+
     def initialize(explain)
       @explain = explain
     end
 
     def operation_method
-      if @explain[:queryPlanner]
-        if @explain[:queryPlanner][:winningPlan][:stage] == 'COLLSCAN'
+      match(@explain) do
+        with(Hash.(queryPlanner: Hash.(winningPlan: Hash.(stage: 'COLLSCAN')))) do
           'Collection Scan'
-        elsif @explain[:queryPlanner][:winningPlan][:stage] == 'FETCH'
-          if @explain[:queryPlanner][:winningPlan][:inputStage][:stage] == 'IXSCAN'
-            "Index Scan (Index Name: #{@explain[:queryPlanner][:winningPlan][:inputStage][:indexName]})"
-          end
         end
-      else
-        'Unknown'
+        with(Hash.(queryPlanner: Hash.(winningPlan: Hash.(stage: 'FETCH', inputStage: Hash.(stage: 'IXSCAN', indexName: index_name ))))) do
+          "Index Scan (Index Name: #{index_name})"
+        end
       end
+    rescue PatternMatch::NoMatchingPatternError
+      'Unknown'
     end
 
     def execution_stats
-      if @explain[:executionStats]
-        { n_returned: @explain[:executionStats][:nReturned], execution_time_millis: @explain[:executionStats][:executionTimeMillis], total_keys_examined: @explain[:executionStats][:totalKeysExamined], total_docs_examined: @explain[:executionStats][:totalDocsExamined] }
-      else
-        { n_returned: nil, execution_time_millis: nil, total_keys_examined: nil, total_docs_examined: nil }
+      match(@explain) do
+        with(Hash.(executionStats: Hash.(nReturned: n, executionTimeMillis: msec, totalKeysExamined: keys, totalDocsExamined: docs))) do
+          { n_returned: n, execution_time_millis: msec, total_keys_examined: keys, total_docs_examined: docs }
+        end
       end
+    rescue PatternMatch::NoMatchingPatternError
+      { n_returned: nil, execution_time_millis: nil, total_keys_examined: nil, total_docs_examined: nil }
     end
   end
 end
