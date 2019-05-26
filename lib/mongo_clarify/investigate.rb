0ddc6d644da9f70bb7744dcbf4390e8f73e3dc9d
morihirok <omnimani3@gmail.com>
2019-05-26 23:51:13 +0900

    exclude Style/LambdaCall from pattern match code

--- lib/mongo_clarify/investigate.rb
+++ lib/mongo_clarify/investigate.rb
@@ -12,10 +12,10 @@ module MongoClarify
 
     def operation_method
       match(@explain) do
-        with(Hash.call(queryPlanner: Hash.call(winningPlan: Hash.call(stage: 'COLLSCAN')))) do
+        with(Hash.(queryPlanner: Hash.(winningPlan: Hash.(stage: 'COLLSCAN')))) do
           'Collection Scan'
         end
-        with(Hash.call(queryPlanner: Hash.call(winningPlan: Hash.call(stage: 'FETCH', inputStage: Hash.call(stage: 'IXSCAN', indexName: index_name))))) do
+        with(Hash.(queryPlanner: Hash.(winningPlan: Hash.(stage: 'FETCH', inputStage: Hash.(stage: 'IXSCAN', indexName: index_name))))) do
           "Index Scan (Index Name: #{index_name})"
         end
       end
@@ -25,7 +25,7 @@ module MongoClarify
 
     def execution_stats
       match(@explain) do
-        with(Hash.call(executionStats: Hash.call(nReturned: n, executionTimeMillis: msec, totalKeysExamined: keys, totalDocsExamined: docs))) do
+        with(Hash.(executionStats: Hash.(nReturned: n, executionTimeMillis: msec, totalKeysExamined: keys, totalDocsExamined: docs))) do
           {
             n_returned: n,
             execution_time_millis: msec,
